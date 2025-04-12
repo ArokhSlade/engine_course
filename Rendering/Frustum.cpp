790c9c737f7ef20019a43003e6e73e06c0e1a16b
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2025-04-12 11:51:24 +0200

    compute planes

--- Rendering/Frustum.cpp
+++ Rendering/Frustum.cpp
@@ -31,22 +31,55 @@ void Frustum::ConstructFrustum(XMMATRIX projectionMatrix, XMMATRIX viewMatrix)
 	XMStoreFloat4x4(&frustumMatrix, finalMatrix);
 
 	// near plane
+	m_planes[0][0] = frustumMatrix._14 + frustumMatrix._13;
+	m_planes[0][1] = frustumMatrix._24 + frustumMatrix._23;
+	m_planes[0][2] = frustumMatrix._34 + frustumMatrix._33;
+	m_planes[0][3] = frustumMatrix._44 + frustumMatrix._43;
 
 	// far plane
+	m_planes[1][0] = frustumMatrix._14 - frustumMatrix._13;
+	m_planes[1][1] = frustumMatrix._24 - frustumMatrix._23;
+	m_planes[1][2] = frustumMatrix._34 - frustumMatrix._33;
+	m_planes[1][3] = frustumMatrix._44 - frustumMatrix._43;
 
 	// left plane
+	m_planes[2][0] = frustumMatrix._14 + frustumMatrix._11;
+	m_planes[2][1] = frustumMatrix._24 + frustumMatrix._21;
+	m_planes[2][2] = frustumMatrix._34 + frustumMatrix._31;
+	m_planes[2][3] = frustumMatrix._44 + frustumMatrix._41;
 
 	// right plane
+	m_planes[3][0] = frustumMatrix._14 - frustumMatrix._11;
+	m_planes[3][1] = frustumMatrix._24 - frustumMatrix._21;
+	m_planes[3][2] = frustumMatrix._34 - frustumMatrix._31;
+	m_planes[3][3] = frustumMatrix._44 - frustumMatrix._41;
 
 	// bottom plane
+	m_planes[4][0] = frustumMatrix._14 + frustumMatrix._12;
+	m_planes[4][1] = frustumMatrix._24 + frustumMatrix._22;
+	m_planes[4][2] = frustumMatrix._34 + frustumMatrix._32;
+	m_planes[4][3] = frustumMatrix._44 + frustumMatrix._42;
 
 	// top plane
+	m_planes[5][0] = frustumMatrix._14 - frustumMatrix._12;
+	m_planes[5][1] = frustumMatrix._24 - frustumMatrix._22;
+	m_planes[5][2] = frustumMatrix._34 - frustumMatrix._32;
+	m_planes[5][3] = frustumMatrix._44 - frustumMatrix._42;
 
 	// normalize planes
+	for (int i = 0; i < 6; ++i) {
+		float length = sqrtf(	m_planes[i][0] * m_planes[i][0] + 
+								m_planes[i][1] * m_planes[i][1] + 
+								m_planes[i][2] * m_planes[i][2]);
+		m_planes[i][0] /= length;
+		m_planes[i][1] /= length;
+		m_planes[i][2] /= length;
+	}
+
 
 }
 
 bool Frustum::IsSphereInsideFrustum(float x, float y, float z, float radius)
 {
-
+	return true;
 }
