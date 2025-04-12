217771a9625d2d3f73df28915ba9e4b1d2f1a6a0
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2025-04-12 10:34:32 +0200

    Frustum header contents

--- Rendering/Frustum.h
+++ Rendering/Frustum.h
@@ -1,5 +1,18 @@
 #include <DirectXMath.h>
+using namespace DirectX;
+
 class Frustum
 {
+public:
+	Frustum();
+	
+	void Initialize(float screenDepth);
+	void ConstructFrustum(XMMATRIX projectionMatrix, XMMATRIX viewMatrix);
+
+	bool IsSphereInsideFrustum(float x, float y, float z, float radius);
+
+private:
+	float m_screenDepth;
+	float m_planes[6][4];
 };
 
