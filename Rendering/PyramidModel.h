1e77770829b82f0ab4dbb4d7ed6e4f640182f9f0
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2025-04-12 16:20:14 +0200

    added pyramid model class

---
+++ Rendering/PyramidModel.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "d3d11.h"
+#include "CustomStruct.h"
+
+class PyramidModel
+{
+
+public:
+	PyramidModel();
+	~PyramidModel();
+
+	bool Initialize(ID3D11Device* device);
+	void Shutdown();
+	void Render(ID3D11DeviceContext* deviceContext);
+
+	int GetIndexCount();
+
+private:
+	ID3D11Buffer* m_vertexBuffer, * m_indexBuffer;
+	int m_vertexCount, m_indexCount;
+};
