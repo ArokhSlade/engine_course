adf8959db22a983a417932cea72e4f4196707051
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2025-04-12 10:57:20 +0200

    construct frustum logic outline

--- Rendering/Frustum.cpp
+++ Rendering/Frustum.cpp
@@ -1,1 +1,46 @@
 #include "Frustum.h"
+
+Frustum::Frustum() {}
+
+void Frustum::Initialize(float screenDepth)
+{
+	m_screenDepth = screenDepth;
+}
+
+void ConstructFrustum(XMMATRIX projectionMatrix, XMMATRIX viewMatrix) 
+{
+	XMFLOAT4X4 proMatrix; //projectionMatrix in another format
+	XMFLOAT4X4 frustumMatrix;
+	float zMin, zMax;
+
+	XMMATRIX finalMatrix;
+
+	XMStoreFloat4x4(&proMatrix, projectionMatrix);
+
+
+
+
+	projectionMatrix = XMLoadFloat4x4(&proMatrix);
+	finalMatrix = XMMatrixMultiply(viewMatrix, projectionMatrix);
+
+	XMStoreFloat4x4(&frustumMatrix, finalMatrix);
+	// near plane
+
+	// far plane
+
+	// left plane
+
+	// right plane
+
+	// bottom plane
+
+	// top plane
+
+	// normalize planes
+
+}
+
+bool IsSphereInsideFrustum(float x, float y, float z, float radius) 
+{
+
+}
