e98b38a5bc9524319826c60ecb8ca6acd5af6526
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2025-04-14 16:08:09 +0200

    wip debugging model initialization

--- Rendering/ModelListClass.cpp
+++ Rendering/ModelListClass.cpp
@@ -27,26 +27,27 @@ bool ModelListClass::Initialize(int numSpheres, int numCubes, int numPyramids)
 	// Seed the random generator with the current time.
 	srand((unsigned int)time(NULL));
 
-	int modelCounts[3] = { numSpheres, numCubes, numPyramids };
-
+	const int modelTypeCount = static_cast<int>(ModelType::ENUM_SIZE);
+	int modelCounts[modelTypeCount] = { numSpheres, numCubes, numPyramids };
 	// Go through all the models and randomly generate the model color and position.
-	for (int modelTypeIndex = 0 ; modelTypeIndex < static_cast<int>(ModelType::ENUM_SIZE) ; modelTypeIndex++)
+	int totalModelIndex = 0;
+	for (int modelTypeIndex = 0 ; modelTypeIndex < modelTypeCount ; modelTypeIndex++)
 	{
-		for (int modelIndex = 0 ; modelIndex < modelCounts[modelIndex]; modelIndex++)
+		for (int modelIndex = 0 ; modelIndex < modelCounts[modelIndex]; modelIndex++, totalModelIndex++)
 		{
-			m_ModelInfoList[modelIndex].modelType = static_cast<ModelType>(modelTypeIndex);
+			m_ModelInfoList[totalModelIndex].modelType = static_cast<ModelType>(modelTypeIndex);
 
 			// Generate a random color for the model.
 			red = (float)rand() / RAND_MAX;
 			green = (float)rand() / RAND_MAX;
 			blue = (float)rand() / RAND_MAX;
 
-			m_ModelInfoList[modelIndex].color = XMFLOAT4(red, green, blue, 1.0f);
+			m_ModelInfoList[totalModelIndex].color = XMFLOAT4(red, green, blue, 1.0f);
 
 			// Generate a random position in front of the viewer for the mode.
-			m_ModelInfoList[modelIndex].positionX = (float)rand() / RAND_MAX * 100.f;
-			m_ModelInfoList[modelIndex].positionY = (float)rand() / RAND_MAX * 100.f;
-			m_ModelInfoList[modelIndex].positionZ = (float)rand() / RAND_MAX * 100.f;
+			m_ModelInfoList[totalModelIndex].positionX = (float)rand() / RAND_MAX * 100.f;
+			m_ModelInfoList[totalModelIndex].positionY = (float)rand() / RAND_MAX * 100.f;
+			m_ModelInfoList[totalModelIndex].positionZ = (float)rand() / RAND_MAX * 100.f;
 		}
 	}
 
