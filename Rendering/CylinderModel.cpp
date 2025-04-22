0f5aae7df127a41f072a3bb64ac0a6b46b5b2013
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2025-04-22 15:14:50 +0200

    added other cylinde functions

--- Rendering/CylinderModel.cpp
+++ Rendering/CylinderModel.cpp
@@ -233,23 +233,59 @@ bool CylinderModel::Initialize(ID3D11Device* device)
 
 void CylinderModel::Shutdown()
 {
+	// Release the index buffer.
+	if (m_indexBuffer)
+	{
+		m_indexBuffer->Release();
+		m_indexBuffer = 0;
+	}
+
+	// Release the vertex buffer.
+	if (m_vertexBuffer)
+	{
+		m_vertexBuffer->Release();
+		m_vertexBuffer = 0;
+	}
+
+	if (m_vertexList)
+	{
+		delete[] m_vertexList;
+		m_vertexList = 0;
+	}
+
+	return;
 }
 
 void CylinderModel::Render(ID3D11DeviceContext* deviceContext)
 {
+	unsigned int stride;
+	unsigned int offset;
+
+	// Set vertex buffer stride and offset.
+	stride = sizeof(ColorVertexType);
+	offset = 0;
+
+	// Set the vertex buffer to active in the input assembler so it can be rendered.
+	deviceContext->IASetVertexBuffers(0, 1, &m_vertexBuffer, &stride, &offset);
+
+	// Set the index buffer to active in the input assembler so it can be rendered.
+	deviceContext->IASetIndexBuffer(m_indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+
+	// Set the type of primitive that should be rendered from this vertex buffer, in this case triangles.
+	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 }
 
 int CylinderModel::GetIndexCount()
 {
-	return 0;
+	return m_indexCount;
 }
 
 VectorType* CylinderModel::GetVertexList()
 {
-	return nullptr;
+	return m_vertexList;
 }
 
 int CylinderModel::GetVertexCount()
 {
-	return 0;
+	return m_vertexCount;
 }
